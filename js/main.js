82b37917f2eac328bd3d95705be4cb2d0a1d24d1
Hussam Nseir <hnseir@satpon.com>
2013-04-01 15:27:44 -0500

    Using Native Notification

--- js/main.js
+++ js/main.js
@@ -14,11 +14,23 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+    	var self = this;
+        this.store = new MemoryStore();{
+        	self.showAlert('Store Initialized', 'Info');
+        });
         //this.store = new LocalStorageStore();
         //this.store = new WebSqlStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+    
+    showAlert: function (message, title) {
+    	if (navigator.notification) {
+    	navigator.notification.alert(message, null, title, 'OK');
+    	} else {
+    	alert(title ? (title + ": " + message) : message);
+    	}
+    },
+    
 
 };
 
