e3a94d09f4dfee7dcfda0a93d63040745ad62f86
Hussam Nseir <hnseir@satpon.com>
2013-04-02 13:51:24 -0500

    Add Style & Scroll

--- js/main.js
+++ js/main.js
@@ -1,63 +1,27 @@
 var app = {
 
-   // findByName: function() {
-       // console.log('findByName');
-       // this.store.findByName($('.search-key').val(), function(employees) {
-          //  var l = employees.length;
-           // var e;
-           // $('.employee-list').empty();
-           // for (var i=0; i<l; i++) {
-               // e = employees[i];
-                //$('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            //}
-       // });
-    //},
-    
-    findByName: function() {
-    	var self = this;
-    	this.store.findByName($('.search-key').val(), function(employees) {
-    	$('.employee-list').html(self.employeeLiTpl(employees));
-    	});
-    	},
-
-    
     showAlert: function (message, title) {
-    	if (navigator.notification) {
-    		navigator.notification.alert(message, null, title, 'OK');
-		} else {
-			alert(title ? (title + ": " + message) : message);
-		}
-	},
-	
-	//renderHomeView: function() {
-		//var html =
-				//"<div class='header'><h1>Home</h1></div>" +
-				//"<div class='search-view'>" +
-				//"<input class='search-key'/>" +
-				//"<ul class='employee-list'></ul>" +
-				//"</div>"
-		//$('body').html(html);
-		//$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	//},
-
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
-    initialize: function() {
-    	this.homeTpl = Handlebars.compile($("#home-tpl").html());
-    	this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-    	var self = this;
-    	this.store = new MemoryStore(function() {
-        	self.showAlert('Store Initialized', 'Info');
-    		self.renderHomeView();
-    	});
-    	//this.store = new MemoryStore();
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+    
+    initialize: function () {
+        var self = this;
+        this.store = new MemoryStore(function () {
+            self.showAlert('Hello from Hussam', 'Greetings');
+            $('body').html(new HomeView(self.store).render().el);
+            //self.renderHomeView();
+        });
+        //this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        //this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());        
+        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
+        //this.store = new MemoryStore();
        //this.store = new LocalStorageStore();
-       //this.store = new WebSqlStore();
-    	$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	}
+        //this.store = new WebSqlStore();
+    }
 };
 
 app.initialize();
